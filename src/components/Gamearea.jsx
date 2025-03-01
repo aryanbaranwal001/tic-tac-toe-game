09339cc0f2d5947e39e326f87dcd6c8f1a4699a5
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-01 18:00:08 +0530

    now Xs and Os are coming nicely and in order

--- src/components/Gamearea.jsx
+++ src/components/Gamearea.jsx
@@ -22,53 +22,52 @@ function Gamearea() {
       setLayoutComponents(() =>{
         return layout
       })
-      
       // eslint-disable-next-line react-hooks/exhaustive-deps
       player="PlayerX"
       
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+  
     
     
     
-function handleClick(cellid) {
-  let value1 = null;
-  const layoutTemp = Object.values(myCellData).map(cell =>{
-
-
-    if (cell.cellid === cellid && player==="PlayerX") {
-      player = "PlayerO"
-      value1 = (
-        <div class="relative w-32 h-32 flex items-center justify-center bg-gray-700">
+    function handleClick(cellid1) {
+      let value1 = null;
+      let cellIdUpdate = null;
+      const layoutTemp = Object.values(myCellData).map(cell =>{
+        
+        
+        if (cell.cellid === cellid1 && player==="PlayerX") {
+          cellIdUpdate = cell.cellid
+          player = "PlayerO"
+          value1 = (
+            <div class="relative w-32 h-32 flex items-center justify-center bg-gray-700">
           <div class="absolute w-36 h-[10px] bg-amber-400 rotate-45"></div>
           <div class="absolute w-36 h-[10px] bg-amber-400 -rotate-45"></div>
         </div>      
       );
-      return (
-        {value1}        
-      )
-    } else if (cell.cellid === cellid && player==="PlayerO") {
+      return value1
+    } else if (cell.cellid === cellid1 && player==="PlayerO") {
+      cellIdUpdate = cell.cellid
       player = "PlayerX"
       value1 = (
         <div className="box-border p-3 bg-gray-700 flex h-32 w-32">
           <div class="flex border-[10px] bg-gray-700 border-amber-400 rounded-full w-full h-full"></div>
         </div>       
       );
-      return (
-        {value1}        
-      )
+      return value1
     } else {
       return (
-        myCellData[cellid].value 
+        myCellData[cell.cellid].value
       )
     }
     
   })
-
-myCellData[cellid].value = (value1)
-console.log(layoutTemp)
-
-setLayoutComponents(layoutTemp)
+  
+  myCellData[cellIdUpdate].value = value1;
+  
+  console.log(layoutTemp)
+  setLayoutComponents(layoutTemp)
 }
 
 
