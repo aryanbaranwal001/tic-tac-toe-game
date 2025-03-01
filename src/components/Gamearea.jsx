143b53db93d9027a77130e617cdd0e437fbad060
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-01 17:16:38 +0530

    clicks are working alternatively, will work on storing click data

--- src/components/Gamearea.jsx
+++ src/components/Gamearea.jsx
@@ -4,31 +4,59 @@ import { useEffect } from "react";
 
 
 function Gamearea() {
-  const [layoutComponents, setLayoutComponents] = useState(null);
-  const 
+  let player = ""
+  const [layoutComponents, setLayoutComponents] = useState([]);
+  const [layoutData, setLayoutData] = useState(cellData);
 
   useEffect(()=>{
     const layout = Object.values(cellData).map(cell =>{
         return (
-          <button key={cell.cellid} disabled={cell.status} onClick={() => handleClick(cell.cellid)} className="bg-gray-700 flex h-32 w-32 cursor-pointer"></button> 
+          <button key={cell.cellid}  onClick={() => handleClick(cell.cellid)} className="bg-gray-700 flex h-32 w-32 cursor-pointer"></button> 
         )
       })
       setLayoutComponents(() =>{
         return layout
       })
-  }, [])
-
-
-
+      
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      player="PlayerX"
+      
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    
+    
+    
 function handleClick(cellid) {
-  Object.values(layoutComponents).map(cell =>{
-    return (
-      <button key={cell.cellid} disabled={cell.status} onClick={() => handleClick(cell.cellid)} className="bg-gray-700 flex h-32 w-32 cursor-pointer"></button> 
-    )
+  console.log(cellid, player)
+  const layoutTemp = Object.values(cellData).map(cell =>{
+
+    if (cell.cellid === cellid && player==="PlayerX") {
+      player = "PlayerO"
+      return (
+        <div class="relative w-32 h-32 flex items-center justify-center bg-gray-700">
+          <div class="absolute w-36 h-[10px] bg-amber-400 rotate-45"></div>
+          <div class="absolute w-36 h-[10px] bg-amber-400 -rotate-45"></div>
+        </div>        
+      )
+    } else if (cell.cellid === cellid && player==="PlayerO") {
+      player = "PlayerX"
+      return (
+        <div className="box-border p-3 bg-gray-700 flex h-32 w-32">
+          <div class="flex border-[10px] bg-gray-700 border-amber-400 rounded-full w-full h-full"></div>
+        </div>        
+      )
+    } else {
+      return (
+        <button key={cell.cellid} onClick={() => handleClick(cell.cellid)} className="bg-gray-700 flex h-32 w-32 cursor-pointer"></button> 
+      )
+    }
+    
   })
 
 
+console.log(layoutTemp)
 
+setLayoutComponents(layoutTemp)
 }
 
 
