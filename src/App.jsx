2b9e8500cd9ed9db74f0e27088ccd8882f88730f
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-01 14:20:09 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  
+  return (
+    <>
+      <div className='flex'>
+        <div className='h-[30px] w-[30px] bg-amber-400'>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
